8c76ad1f93832a6d6817a3b841ee9e9b8db1527e
fanzewei <fanzewei@yongche.com>
2021-09-09 11:10:49 +0800

    验证规则添加默认查询条件ok

--- src/Form/FormItem.php
+++ src/Form/FormItem.php
@@ -474,7 +474,7 @@ class FormItem extends Component
     public function unique($unique = true, $table = null, $field = null, $fieldName = null, $extended = '')
     {
         if ($unique && !empty($table) && !empty($field) && !empty($fieldName)) {
-            $this->serveCreationRules(['unique:' . $table]);
+            $this->serveCreationRules(['unique:' . $table . ',' . $field . ',NULL,id,deleted_at,NULL' . $extended]);
             $this->serveUpdateRules(['unique:' . $table . ',' . $field . ',{{id}},id,deleted_at,NULL' . $extended]);
             $this->serveRulesMessage(['unique' => $fieldName . ' 已存在']);
         }
